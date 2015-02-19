b5fe32121d212db25653a6cb2d5e9a9d00d85f29
Danil Skachkov <danil.skachkov@speaktoit.com>
2015-02-19 18:37:55 +0600

    Working sample

--- Assets/ApiAiModule.cs
+++ Assets/ApiAiModule.cs
@@ -1,4 +1,24 @@
-﻿using UnityEngine;
+//
+// API.AI Unity SDK Sample
+// =================================================
+//
+// Copyright (C) 2015 by Speaktoit, Inc. (https://www.speaktoit.com)
+// https://www.api.ai
+//
+// ***********************************************************************************************************************
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+//
+// ***********************************************************************************************************************
+
+using UnityEngine;
 using UnityEngine.UI;
 using System;
 using System.IO;
@@ -6,16 +26,17 @@ using System.Collections;
 using System.Reflection;
 using fastJSON;
 using ApiAiSDK;
-using ApiAiSDK.model;
+using ApiAiSDK.Model;
+using ApiAiSDK.Unity;
 
 public class ApiAiModule : MonoBehaviour
 {
 
 	public Text answerTextField;
-	private ApiAi apiAi;
+	public Text inputTextField;
+	private ApiAiUnity apiAiUnity;
 	private AudioSource aud;
 	public AudioClip listeningSound;
-	private volatile bool recording = false;
 	BinaryWriter writer;
 	FileStream fs;
 	int blocksWrote;
@@ -24,158 +45,122 @@ public class ApiAiModule : MonoBehaviour
 	// Use this for initialization
 	IEnumerator Start()
 	{
-
+		// check access to the Microphone
 		yield return Application.RequestUserAuthorization (UserAuthorization.Microphone);
 		if (!Application.HasUserAuthorization (UserAuthorization.Microphone)) {
 			throw new NotSupportedException ("Microphone using not authorized");
-		} 
+		}
 
 		const string SUBSCRIPTION_KEY = "cb9693af-85ce-4fbf-844a-5563722fc27f";
 		const string ACCESS_TOKEN = "3485a96fb27744db83e78b8c4bc9e7b7";
 
-		var config = new AIConfiguration (SUBSCRIPTION_KEY, ACCESS_TOKEN, "en");
-		config.DebugMode = false;
+		var config = new AIConfiguration (SUBSCRIPTION_KEY, ACCESS_TOKEN, SupportedLanguage.English);
 
-		apiAi = new ApiAi (config);
+		apiAiUnity = new ApiAiUnity ();
+		apiAiUnity.Initialize (config);
+
+		apiAiUnity.OnError += HandleOnError;
+		apiAiUnity.OnResult += HandleOnResult;
+	}
+
+	void HandleOnResult(object sender, AIResponseEventArgs e)
+	{
+		var aiResponse = e.Response;
+		if (aiResponse != null) {
+			Debug.Log (aiResponse.Result.ResolvedQuery);
+			var outText = fastJSON.JSON.ToJSON (aiResponse,
+			                                   new JSONParameters
+			                                   {
+													UseExtensions = false,
+													SerializeNullValues = false,
+													EnableAnonymousTypes = true
+											   });
+			
+			Debug.Log (outText);
+			
+			answerTextField.text = outText;
+			
+		} else {
+			Debug.LogError ("Response is null");
+		}
+	}
+	
+	void HandleOnError(object sender, AIErrorEventArgs e)
+	{
+		Debug.LogException (e.Exception);
+		answerTextField.text = e.Exception.Message;
 	}
 	
 	// Update is called once per frame
 	void Update()
 	{
-	
+		
 	}
-
+	
 	public void PluginInit()
 	{
-
+		
 	}
-
+	
 	public void StartListening()
 	{
 		Debug.Log ("StartListening");
-
+			
 		if (answerTextField != null) {
 			answerTextField.text = "Listening...";
 		}
-
-		foreach (var mic in Microphone.devices) {
-			int min, max;
-			Microphone.GetDeviceCaps (mic, out min, out max);
-			Debug.Log (mic + ": " + min + " - " + max);
-		}
-
-
-
+			
 		aud = GetComponent<AudioSource> ();
-
-		aud.PlayOneShot (listeningSound);
-
-		//aud.clip = Microphone.Start (null, true, 20, 16000);
-
-		recording = true;
-
+		apiAiUnity.StartListening (aud);
+	
 	}
 	
 	public void StopListening()
 	{
-		Debug.Log ("StopListening");
-
-
-		recording = false;
-
-
-		if (answerTextField != null) {
-			answerTextField.text = "";
-		}
-
 		try {
-			Microphone.End (null);
+			Debug.Log ("StopListening");
 
-			var samples = new float[aud.clip.samples];
-			aud.clip.GetData (samples, 0);
-
-			Debug.Log ("samples: " + samples.Length);
-
-			Debug.Log ("channels:" + aud.clip.channels);
-			Debug.Log ("freq:" + aud.clip.frequency);
-
-		
-						
-			var aiResponse = apiAi.voiceRequest (samples);
-
-			if (aiResponse != null) {
-				Debug.Log (aiResponse.Result.resolvedQuery);
-				var outText = fastJSON.JSON.ToJSON (aiResponse, 
-							new JSONParameters { 
-								UseExtensions = false,  
-								SerializeNullValues = false,
-								EnableAnonymousTypes = true
-							});
-						
-				Debug.Log (outText);
-						
-				answerTextField.text = outText;
-						
-			} else {
-				Debug.LogError ("Response is null");
+			if (answerTextField != null) {
+				answerTextField.text = "";
 			}
-
+			
+			apiAiUnity.StopListening ();
 		} catch (Exception ex) {
 			Debug.LogException (ex);
 		}
 	}
-
-//	public void OnAudioFilterRead(float[] data,int channels){
-//		Debug.Log("!");
-//		counter++;
-//		if(recording && writer != null && data != null){
-//			var byteArray = new byte[data.Length * 4];
-//			Buffer.BlockCopy(data, 0, byteArray, 0, byteArray.Length);
-//			writer.Write(byteArray);
-//			blocksWrote++;
-//		}
-//	}
-
-	private void StartSoundFile()
-	{
-		fs = new FileStream ("recorded_sound.raw", FileMode.Create);
-		writer = new BinaryWriter (fs);
-
-	}
-
-	private void StopSoundFile()
-	{
-		fs.Close ();
-		writer.Close ();
-		fs = null;
-		writer = null;
-
-		Debug.Log ("wrote/counter:" + blocksWrote + " " + counter);
-	}
 	
-	public void SendText(string text)
+	public void SendText()
 	{
+		var text = inputTextField.text;
+
 		Debug.Log (text);
 
-		Debug.Log (System.Environment.Version);
-
-		AIResponse response = apiAi.textRequest (text);
+		AIResponse response = apiAiUnity.TextRequest (text);
 
 		if (response != null) {
-			Debug.Log (response.Result.resolvedQuery);
-			var outText = fastJSON.JSON.ToJSON (response, new JSONParameters { 
-				UseExtensions = false,  
-				SerializeNullValues = false,
-				EnableAnonymousTypes = true
-			});
+			Debug.Log ("Resolved query: " + response.Result.ResolvedQuery);
+			var outText = fastJSON.JSON.ToJSON (response, new JSONParameters
+            {
+                UseExtensions = false,
+                SerializeNullValues = false,
+                EnableAnonymousTypes = true
+            });
 
-			Debug.Log (outText);
+			Debug.Log ("Result: " + outText);
 
 			answerTextField.text = outText;
-
 		} else {
 			Debug.LogError ("Response is null");
 		}
 
+	}
+
+	public void StartNativeRecognition(){
+		try {
+			apiAiUnity.StartNativeRecognition();
+		} catch (Exception ex) {
+			Debug.LogException (ex);
+		}
 	}
 }
